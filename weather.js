7cd246b20ed96ee78c660ffb25049a7e61ca3bc6
Geunmo Kim <kimgeunmo@gmail.com>
2021-04-25 20:44:11 +0200

    adding weather with API

---
+++ weather.js
@@ -0,0 +1,39 @@
+const API_KEY = "396cdae68a7936f2d7a525a9e8f5e27f";
+const COORDS = "coords";
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+  console.log("Can't handle geo location");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    // getWeather
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
