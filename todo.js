5666505c61c24b7f2a872a685a4489797568d83c
Geunmo Kim <kimgeunmo@gmail.com>
2021-04-25 13:43:23 +0200

    after error

--- todo.js
+++ todo.js
@@ -10,7 +10,7 @@ function deleteToDo(event) {
   const btn = event.target;
   const li = btn.parentNode;
   toDoList.removeChild(li);
-  const cleanToDos = toDos.filter(function (todo) {
+  const cleanToDos = toDos.filter(function (toDo) {
     return toDo.id !== parseInt(li.id);
   });
   toDos = cleanToDos;
